6b690cebb5827fda37460aee82b57b085f468b77
theo <tgueuret@live.fr>
2024-07-10 17:05:49 +0200

    adding a Board.py file which contains the class Board, which represents the Board of the game

---
+++ Legum/Board.py
@@ -0,0 +1,38 @@
+# Board.py
+
+# Imports
+import numpy as np
+
+
+class Board:
+
+    EMPTY = 0
+
+    WHITE_PAWN = 1
+    WHITE_KNIGHT = 2
+    WHITE_BISHOP = 3
+    WHITE_ROOK = 4
+    WHITE_QUEEN = 5
+    WHITE_KING = 6
+
+    BLACK_PAWN = 7
+    BLACK_KNIGHT = 8
+    BLACK_BISHOP = 9
+    BLACK_ROOK = 10
+    BLACK_QUEEN = 11
+    BLACK_KING = 12
+
+    def __init__(self):
+        self.board = np.zeros(shape=(8, 8), dtype=int)
+
+    def load_to_default(self):
+        self.board = np.array([
+            [Board.BLACK_ROOK, Board.BLACK_KNIGHT, Board.BLACK_BISHOP, Board.BLACK_QUEEN, Board.BLACK_KING, Board.BLACK_BISHOP, Board.BLACK_KNIGHT, Board.BLACK_ROOK],
+            [Board.BLACK_PAWN, Board.BLACK_PAWN, Board.BLACK_PAWN, Board.BLACK_PAWN, Board.BLACK_PAWN, Board.BLACK_PAWN, Board.BLACK_PAWN, Board.BLACK_PAWN],
+            [Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY],
+            [Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY],
+            [Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY],
+            [Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY, Board.EMPTY],
+            [Board.WHITE_PAWN, Board.WHITE_PAWN, Board.WHITE_PAWN, Board.WHITE_PAWN, Board.WHITE_PAWN, Board.WHITE_PAWN, Board.WHITE_PAWN, Board.WHITE_PAWN],
+            [Board.WHITE_ROOK, Board.WHITE_KNIGHT, Board.WHITE_BISHOP, Board.WHITE_QUEEN, Board.WHITE_KING, Board.WHITE_BISHOP, Board.WHITE_KNIGHT, Board.WHITE_ROOK]
+        ])
