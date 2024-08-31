7e9a64a7ec839ed17478d2eca7e5afb4271807d9
Théo <tgueuret@live.fr>
2024-08-31 22:26:36 +0200

    testinggg

--- test.py
+++ test.py
@@ -1,35 +1,7 @@
-import numpy as np
-
-position = (3, 3)
 
 
-first_part = []
-for i in range(1, 8):
-    first_part.append((position[0] - i, position[1] - i))
-    first_part.append((position[0] - i, position[1] + i))
-    first_part.append((position[0] + i, position[1] - i))
-    first_part.append((position[0] + i, position[1] + i))
-first_part = np.array(first_part)
-first_part = first_part[first_part[:, 0] >= 0]
-first_part = first_part[first_part[:, 0] < 8]
-first_part = first_part[first_part[:, 1] >= 0]
-first_part = first_part[first_part[:, 1] < 8]
+from Legum.Board import Board
 
-second_part = []
-for i in range(1, 8):
-    second_part.append((position[0] - i, position[1]))
-    second_part.append((position[0] + i, position[1]))
-    second_part.append((position[0], position[1] - i))
-    second_part.append((position[0], position[1] + i))
-second_part = np.array(second_part)
-second_part = second_part[second_part[:, 0] >= 0]
-second_part = second_part[second_part[:, 0] < 8]
-second_part = second_part[second_part[:, 1] >= 0]
-second_part = second_part[second_part[:, 1] < 8]
-
-
-print(f"First part: {first_part}")
-print(f"Second part: {second_part}")
-
-
-
+board = Board()
+board.set_to_default()
+board.print_board_to_console()
