f672e1241193f0bd0c08532de9838e386fe77dcf
Théo <tgueuret@live.fr>
2024-08-31 22:26:07 +0200

    adding colors

--- Legum/constants.py
+++ Legum/constants.py
@@ -3,3 +3,21 @@ WHITE_COLOR = 1
 BLACK_COLOR = 2
 EMPTY = 0
 BOARD_SHAPE = (8, 8)
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+MAGENTA = '\033[95m'
+CYAN = '\033[96m'
+WHITE = '\033[97m'
+RESET = '\033[0m'
+
+PIECES_LETTER_REPR = {
+    "King": "K",
+    "Queen": "Q",
+    "Rook": "R",
+    "Bishop": "B",
+    "Knight": "N",
+    "Pawn": "P"
+}
