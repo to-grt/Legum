6630827433d6648840353dda17e7ca6ea70848e8
Théo Gueuret <tgueuret@live.fr>
2025-11-17 20:33:29 +0100

    clean slate

--- test.py
+++
@@ -1,7 +0,0 @@
-
-
-from Legum.Board import Board
-
-board = Board()
-board.set_to_default()
-board.print_board_to_console()
--- tests/test_Piece.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from Legum.Pieces.Piece import Piece
-
-
-class TestPiece(unittest.TestCase):
-
-    def test_piece_not_instantiated(self):
-        with self.assertRaises(Exception):
-            Piece()
