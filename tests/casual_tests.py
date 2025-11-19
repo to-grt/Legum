260efba77f55b21773efcec65415d25325c45a8b
Théo Gueuret <tgueuret@live.fr>
2025-11-19 23:21:08 +0100

    adding gui instantiation

--- tests/casual_tests.py
+++ tests/casual_tests.py
@@ -1,10 +1,14 @@
 from src.components import King, Board
 
+from src.gui import ChessGUI
+
 
 def main():
     board = Board()
     print(board)
 
+    gui = ChessGUI(board_size=board.board_size)
+    gui.run(board)
 
     king = King(board=board, color='white', position=(4, 4), is_alive=True)
     print(king)
